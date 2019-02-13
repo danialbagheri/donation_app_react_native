64ecf78e43b5cbc45d698f7f3615898e3bdef960
Mehrdad Faraji <mehrdadfaraji.cs@gmail.com>
2019-02-13 13:24:45 +0330

    Add react-splash-screen library

--- ios/bloksphere/AppDelegate.m
+++ ios/bloksphere/AppDelegate.m
@@ -9,6 +9,9 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNSplashScreen.h" 
+
+
 
 @implementation AppDelegate
 
@@ -29,6 +32,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];  // here
   return YES;
 }
 
