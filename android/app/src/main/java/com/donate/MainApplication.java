5ae2693befae2ada66f72d079fd06a8a185d1317
Mehrdad Faraji <mehrdadfaraji.cs@gmail.com>
2019-03-25 00:44:58 +0430

    Create login view

--- android/app/src/main/java/com/donate/MainApplication.java
+++ android/app/src/main/java/com/donate/MainApplication.java
@@ -3,6 +3,7 @@ package com.donate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactMaterialKitPackage(),
             new VectorIconsPackage(),
             new FastImageViewPackage(),
             new SplashScreenReactPackage(),
--- features/login/LoginScreen.js
+++ features/login/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View,Button, Text } from "react-native";
+import { View,Button,Text, TextInput} from "react-native";
 
 
 
@@ -17,10 +17,34 @@ import { View,Button, Text } from "react-native";
 class LoginScreen extends React.Component {
     render() {
       return (
-        <View style={{ backgroundColor:"#28433F", flex: 1, alignItems: "flex-start", justifyContent: "center", }}>
-          <Text style = {{color:"white"}}>Email</Text>
-          <Button title="Go to Details"
-          />
+        <View style={{ backgroundColor:"#28433F", flex: 1,alignItems: "flex-start", justifyContent: "center",paddingHorizontal:40 }}>
+         
+          <Text style = {{color:"#FFFFFF",fontSize:20,textAlign:"left"}}>Email</Text>
+         
+          <TextInput style = {{height:40 ,color:'white', borderColor:"#FCFCFC99",borderWidth:1,alignSelf:"stretch",marginVertical:8}} 
+          keyboardType ='email-address'
+          placeholder = "example@gmail.com"
+          placeholderTextColor = '#FCFCFC99'
+          ></TextInput>
+         
+          <Text style = {{color:"#FFFFFF",fontSize:20,textAlign:"left",marginTop:8}}>Password</Text>
+         
+          <TextInput style = {{height:40 , borderColor:"#FCFCFC99",borderWidth:1,alignSelf:"stretch",marginVertical:8}}
+          secureTextEntry = {true}
+          placeholder = "*********"
+          placeholderTextColor = '#FCFCFC99'
+          ></TextInput>
+         
+          <View style={{marginTop:24, borderColor:"white",borderWidth:1,color:"white",alignSelf:"stretch"}}>
+          <Button color="white" title="LOGIN"/>    
+          </View>
+         
+          <Text style = {{color:"#FFFFFF",fontSize:20,textAlign:"left",marginVertical:16,alignSelf:"center"}}>or</Text>
+         
+          <View style={{ borderColor:"#FF0000",borderWidth:1,alignSelf:"stretch"}}>
+          <Button color="#FF0000" title="SIGN IN WITH GOOGLE"/>    
+          </View>
+
         </View>
       );
     }
